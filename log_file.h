c113922a0250469e9f6388110d96dd38d1b7b10d
Roiamato <roiamato@gmail.com>
2021-02-09 20:36:27 +0200

    open employees file and write the function

---
+++ log_file.h
@@ -0,0 +1,7 @@
+#pragma once
+struct iteam {
+	int acctNum; /* account number */
+	char lastName[15]; /* account last name */
+	char firstName[10]; /* account first name */
+	double balance; /* account balance */
+}; /* end structure clientData */
